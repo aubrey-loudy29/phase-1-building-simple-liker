91426e59763a4bd2e8a1e91d5e270399905ea8d8
Aubrey Loudenslager <aubrey.d.webster@gmail.com>
2023-02-24 14:24:00 -0700

    final

--- main.js
+++ main.js
@@ -3,7 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likes = document.querySelectorAll('.like-glyph')
+//picking out the like id that you found from HTML inspect.
+//if youre selecting a class name, us . in the parenthesis. if youre using an id, use # in the parenthesis
+  likes.forEach((pushLike) => {
+    pushLike.addEventListener("click", () => {
 
+      if (pushLike.textContent === EMPTY_HEART) {
+        mimicServerCall()
+        .then(() => {
+          pushLike.textContent = FULL_HEART
+          pushLike.className = 'activated-heart'
+        })
+      }
+      
+      //console.log(pushLike)
+      //^ this is just a midway test to see if what youre 
+      //doing is what you should be doing
+    })
+    
+})
+// to invoke a function, write the function with parenthesis
+
+//console.log(likes)
+//invoke the new function you just wrote
 
 
 
